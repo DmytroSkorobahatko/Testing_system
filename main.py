982ec0a29c0a2376e82885122f3be8a4977b36ed
Dimas <skorobagatko@lic145.kiev.ua>
2020-11-27 12:12:02 +0200

    ans

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ window.geometry('500x400')
 
 window.update()
 
+
 def start_test():
     global text_index
     text_index = 0
@@ -14,48 +15,53 @@ def start_test():
     test_frame.pack(fill=BOTH)
     ans_frame.pack(fill=BOTH, expand=1)
 
-    question_message.config(text=que_text_list[text_index], width=window.winfo_width() - que_mes_x)  # відступ
+    question_message.config(text=que_list[text_index], width=window.winfo_width() - que_mes_x)  # відступ
     question_message.pack(side=TOP, pady=que_mes_y, padx=que_mes_x, fill=X)
-    question_message.bind("<Configure>", lambda e: question_message.configure(width=e.width-10))
+    question_message.bind("<Configure>", lambda e: question_message.configure(width=e.width - 10))
+
+    ans_box.pack()
 
     # little_menu_frame
     little_menu_frame.pack(fill=X, side=BOTTOM)
-    que_next.pack(side=RIGHT, padx=5)
-    que_prev.pack(side=RIGHT, padx=5)
+    next_btn.pack(side=RIGHT, padx=5)
+    #que_prev.pack(side=RIGHT, padx=5)
     menu_btn.pack(side=LEFT, padx=5)
+    finish_btn.pack(expand=1)
 
 
 def finish_test():
     test_frame.pack_forget()
     ans_frame.pack_forget()
     finish_btn.pack_forget()
+    next_btn.pack_forget()
+
+    finish_frame.pack(fill=BOTH, expand=1)
+
+    finish_label.pack(fill=X)
+
+    
+
+def main_menu():
+    test_frame.pack_forget()
+    ans_frame.pack_forget()
     little_menu_frame.pack_forget()
+    finish_frame.pack_forget()
 
     main_menu_frame.pack(fill=BOTH, expand=1)
 
-
 def next_question():
-    global text_index, text_count
+    global text_index, text_count, que_list, test_que_ans_dict
     if text_index < text_count - 1:
         text_index = text_index + 1
-    else:
-        finish_btn.pack(expand=1)
 
     question_message.pack_forget()
+    ans_box.delete(0, END)
+    ans_box.config(height=len(test_que_ans_dict[que_list[text_index]]))
 
-    # до 2 (-)=(перенос строки), НЕ ОБОВ'ЯЗКОВО
-    if que_text_list[text_index][-4:-1] == "(-)":
-        if que_text_list[text_index + 1][-4:-1] == "(-)":
-            que_text_list[text_index + 1] = que_text_list[text_index + 1][:-4] + "\n" + que_text_list[text_index + 2]
-            que_text_list.remove(que_text_list[text_index + 2])
-            text_count -= 1
+    for ans in test_que_ans_dict[que_list[text_index]]:
+        ans_box.insert(END, ans)
 
-        que_text_list[text_index] = que_text_list[text_index][:-4] + "\n" + que_text_list[text_index + 1]
-        que_text_list.remove(que_text_list[text_index + 1])
-        text_count -= 1
-    # уже краще
-
-    question_message.config(text=que_text_list[text_index], width=window.winfo_width() - (que_mes_x + 1))  # відступ
+    question_message.config(text=que_list[text_index], width=window.winfo_width() - (que_mes_x + 1))  # відступ
     question_message.pack(side=TOP, pady=que_mes_y, padx=que_mes_x, fill=X)
 
 
@@ -64,58 +70,91 @@ def prev_question():
     if text_index > 0:
         text_index = text_index - 1
 
+    ans_box.delete(0, END)
+    ans_box.config(height=len(test_que_ans_dict[que_list[text_index]]))
+
+    for ans in test_que_ans_dict[que_list[text_index]]:
+        ans_box.insert(END, ans)
+
     question_message.pack_forget()
-    question_message.config(text=que_text_list[text_index])
+    question_message.config(text=que_list[text_index])
     question_message.pack(side=TOP, pady=10, padx=10, fill=X)
 
 
 # TEST all \/\/\/
 # test frame
 test_frame = Frame()
-test_frame.config(bg="brown")
+test_frame.config(bg="#69c")
 # test_frame.pack()
 # test_frame.pack_forget()
 #
 
 # Question
-question_message = Message(test_frame)
+question_message = Message(test_frame, font=(16))
 que_mes_x = 10
 que_mes_y = 10
 
-que_text_list = open("test/questions/questions.txt").readlines()
-text_count = len(que_text_list)
 text_index = 0
 #
 
-
-# ans
-ans_text = open("test/answer/answers.txt").readlines()
-ans_count = len(ans_text)
-
+# Ans
 ans_frame = Frame()
-ans_frame.config(bg="brown")
-
-ans_listbox = Listbox(ans_frame)
-ans_listbox.config()
-ans_listbox.pack(expand=1)
-for i in ans_text:
-    ans_listbox.insert(0, i)
-
-
-# ans_button = Button(ans_frame)
-# for i in range(0, ans_count):
-#     Button(ans_frame, text=ans_text[i], ).pack(expand=1, side=LEFT, pady=10, padx=10)
+ans_frame.config(bg="#69c")
 #
 
-test_file = open("test/test.txt").readlines()
-i=0
-while test_file[i] != "(end)":
-    while test_file[i] != "\n" :
-        test_file[i]
-        que_ans_list_pair = [[ans_listbox], [ans_listbox]]
-        que_ans_list_pair[] = (que_text_list[i], ans_listbox)
-        i += 1
+test_lines = open("test/test.txt").readlines()
 
+ans_list = []
+que_list = []
+test_que_ans_dict = dict()
+# = open("test/test.txt").readlines()
+i = 0
+while test_lines[i] != "(end)":
+    j = i + 1
+    while test_lines[j] != "\n":
+        test_que_ans_dict[str(test_lines[i].rstrip())] = []
+        ans_list += [test_lines[j]]
+        # print(ans_list)
+        j += 1
+
+    test_que_ans_dict[str(test_lines[i].rstrip())] = ans_list
+    ans_list = []
+    i = j + 1
+
+for key in test_que_ans_dict:
+    que_list += [key]
+
+ans_box = Listbox(ans_frame, height=len(test_que_ans_dict[que_list[text_index]]), bg="#69c", font=(16))
+
+for ans in test_que_ans_dict[que_list[text_index]]:
+    ans_box.insert(END, ans)
+
+ans_box.pack(fill=X)
+
+
+print(test_que_ans_dict)
+text_count = len(que_list)
+
+##############################
+#     print(key)
+#
+# print(test_que_ans)
+# print(que_list)
+# i = 0
+# j = 0
+# while test_lines[i] != "(end)":
+#     j = i
+#     while test_lines[j] != "\n":
+#         print(test_lines[j].rstrip())
+#         j += 1
+#         if test_lines[i] == "\n":
+#             print("Hello")
+#
+#     i = j + 1
+# for lines in test_lines:
+#     line = lines.rstrip()
+#     print(line)
+#############################3
 
 # TEST /\/\/\
 
@@ -129,16 +168,24 @@ start_btn = Button(main_menu_frame, text="start test", command=start_test)
 start_btn.pack(expand=1)
 # Main Menu /\
 
+#
+finish_frame = Frame()
+finish_frame.config(bg="grey")
+
+finish_label = Label(finish_frame)
+finish_label.config(text="Congrat!")
+
+
 # Little Menu \/
 little_menu_frame = Frame()
 little_menu_frame.config(bg="black", borderwidth=4)
 
-que_next = Button(little_menu_frame, text="next", command=next_question, bg="black", fg="white")
-que_prev = Button(little_menu_frame, text="prev", command=prev_question, bg="black", fg="white")
+next_btn = Button(little_menu_frame, text="next", command=next_question, bg="black", fg="white", font=(16))
+#que_prev = Button(little_menu_frame, text="prev", command=prev_question, bg="black", fg="white")
 
-menu_btn = Button(little_menu_frame, text="main menu", command=finish_test, bg="black", fg="white")
+menu_btn = Button(little_menu_frame, text="main menu", command=main_menu, bg="black", fg="white", font=(16))
 
-finish_btn = Button(little_menu_frame, text="Finish Test", command=finish_test, bg="black", fg="white")
+finish_btn = Button(little_menu_frame, text="Finish Test", command=finish_test, bg="black", fg="white", font=(16))
 # little menu /\
 # MENU /\/\/\
 
